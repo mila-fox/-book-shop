0d794acb90b70e21a582755f4e0b7bc851abcd76
mila-fox <luda_grischuk@tut.by>
2022-12-04 09:26:06 +0300

    feat: add cart

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const main = document.getElementById('bookshop-app');
 let books = [];
+let cart = new Set();
 
 function createElement(tag, className) {
     const element = document.createElement(tag);
-    element.classList.add(className);
+    if (className) {
+        element.classList.add(className);
+    }
     return element;
 }
 
@@ -39,8 +42,7 @@ function createCard({ id, author, imageLink, title, price, description }) {
 
     const btnShowMore = createElement('button', 'catalog-card-show');
     btnShowMore.innerHTML = 'Show More';
-    btnShowMore.setAttribute("data-id", id);
-    btnShowMore.addEventListener('click', showMore);
+    btnShowMore.addEventListener('click', showMore(id));
 
     catalogCardImg.appendChild(btnShowMore);
 
@@ -55,6 +57,7 @@ function createCard({ id, author, imageLink, title, price, description }) {
 
     const btnCardAdd = createElement('button', 'catalog-card-add');
     btnCardAdd.innerHTML = 'Add To Bag';
+    btnCardAdd.addEventListener('click', addBookToCart(id));
 
     cardOrder.append(elementPrice, btnCardAdd);
 
@@ -82,37 +85,70 @@ function clearMain() {
     }
 }
 
+function createHeader() {
+    const header = createElement('div', 'book-shop');
+
+    const shoppingName = createElement('h2', 'shopping-name');
+    shoppingName.innerHTML = 'BOOK CATALOG';
+
+    const shoppingButton = createElement('button', 'shopping-button');
+    const shoppingImg = createElement('div', 'shoppinimg');
+    const shoppingCart = createElement('span', 'orderquantity');
+    shoppingCart.innerHTML = '0';
+    shoppingButton.append(shoppingImg, shoppingCart);
+    shoppingButton.addEventListener('click', showCart);
+
+    header.append(shoppingName, shoppingButton);
+    return header;
+}
+
+function addBookToCart(bookId) {
+    return function() {
+        const book = books.find(({ id }) => id === bookId);
+        cart.add(book);
+        const countElement = document.querySelector('.orderquantity');
+        countElement.innerHTML = cart.size;
+    }
+}
+
 async function showBooks() {
     clearMain();
     books = await getBooks();
+    const header = createHeader();
     const cardlist = createCardList();
-    main.appendChild(cardlist);
+    main.append(header, cardlist);
 }
 
-function showMore(e) {
-    const windowInnerHeight = document.documentElement.clientHeight
-    yOffset = window.pageYOffset;
+function showMore(currentId) {
+    return function() {
+        const windowInnerHeight = document.documentElement.clientHeight
+        yOffset = window.pageYOffset;
 
-    const bookId = e.currentTarget.getAttribute('data-id');
-    const overlay = createElement('div', 'main-overlay');
-    overlay.addEventListener('click', closeDescription);
-    const modal = createElement('div', 'show-more-modal');
-    modal.style.top = `${yOffset + windowInnerHeight/2}px`;
-    const modalTitle = createElement('span', 'modal-title')
-    modalTitle.innerHTML = books.find(({ id }) => id == bookId).title
-    const bookdescription = createElement('p', 'book-description');
-    bookdescription.innerHTML = books.find(({ id }) => id == bookId).description;
-    const btnClose = createElement('button', 'description-close');
-    const btnImg = createElement('img', 'btn-img');
-    btnImg.src = './assets/icons/window_close_icon_135015.svg';
-    btnClose.append(btnImg);
-    btnClose.addEventListener('click', closeDescription);
+        const book = books.find(({ id }) => id == currentId);
+        const overlay = createElement('div', 'main-overlay');
+        overlay.addEventListener('click', closeDescription);
 
-    modal.append(btnClose, modalTitle, bookdescription);
-    document.body.style.overflow = 'hidden';
+        const modal = createElement('div', 'show-more-modal');
+        modal.style.top = `${yOffset + windowInnerHeight/2}px`;
+        const modalTitle = createElement('span', 'modal-title')
 
-    main.append(overlay, modal);
+        modalTitle.innerHTML = book.title
+        const bookdescription = createElement('p', 'book-description');
+        bookdescription.innerHTML = book.description;
 
+        const btnClose = createElement('button', 'description-close');
+
+        const btnImg = createElement('img', 'btn-img');
+        btnImg.src = './assets/icons/window_close_icon_135015.svg';
+
+        btnClose.append(btnImg);
+        btnClose.addEventListener('click', closeDescription);
+
+        modal.append(btnClose, modalTitle, bookdescription);
+        document.body.style.overflow = 'hidden';
+
+        main.append(overlay, modal);
+    }
 }
 
 function closeDescription() {
@@ -121,6 +157,210 @@ function closeDescription() {
     const modal = document.querySelector('.show-more-modal');
     modal.remove();
     overlay.remove();
+}
+
+
+function orderBooks() {
+    const btnClose = createElement('button', 'description-close');
+    const btnImg = createElement('img', 'btn-img');
+    btnImg.src = './assets/icons/window_close_icon_135015.svg';
+}
+
+function getOrderTotalSum() {
+    return Array.from(cart.keys()).reduce((sum, { price }) => sum += price, 0);
+}
+
+function setOrderSumTotal() {
+    const orderTotal = document.querySelector('.order-total span');
+    orderTotal.innerHTML = `Total: $${getOrderTotalSum()}`;
+}
+
+function order() {
+    const order = createElement('div', 'order');
+    const orderName = createElement('h2','order-name');
+    orderName.innerHTML = 'ORDER BOOKS';
+
+    const orderTotal = createElement('div', 'order-total');
+    const totalSpan = createElement('span');
+    totalSpan.innerHTML = `Total: $${getOrderTotalSum()}`;
+
+    const orderBtn = createElement('button', 'order-btn');
+    orderBtn.innerHTML = 'Confirm';
+    orderTotal.append(totalSpan, orderBtn);
+    orderBtn.addEventListener('click', showOrderForm);
+
+    order.append(orderName, createOrderList(), orderTotal);
+    return order;
+}
+
+function deleteBookFromOrder(book) {
+    return e => {
+        cart.delete(book);
+        e.currentTarget.closest('.order-card').remove();
+        setOrderSumTotal();
+    }
+}
+
+function createOrder(book) {
+    const { id, author, imageLink, title, price } = book;
+    const card = createElement('li', 'order-card');
+
+    const orderImg = createElement('img', 'order-img');
+    orderImg.src = `./assets/images/${imageLink}`;
+
+    const orderBook = createElement('div', "order-book");
+
+    const orderAuthor = createElement('span', 'order-author');
+    orderAuthor.innerHTML = author;
+
+    const orderTitle = createElement('span', 'order-title');
+    orderTitle.innerHTML = title;
+
+    const orderPrice = createElement('span', 'order-price');
+    orderPrice.innerHTML = `$${price}`;
+
+    const orderCloseBtn = createElement('button', 'order-close');
+    orderCloseBtn.addEventListener('click', deleteBookFromOrder(book));
+    const orderCloseImg = createElement('img', 'order-close-img');
+    orderCloseImg.src = './assets/icons/window_close_icon_135015.svg';
+    orderCloseBtn.append(orderCloseImg);
+
+    orderBook.append(orderAuthor, orderTitle, orderPrice);
+    card.append(orderImg, orderBook, orderCloseBtn);
+
+    return card;
+}
+
+function createOrderList() {
+    const orderListCards = createElement('ul');
+    const cards = Array.from(cart.keys()).map(book => createOrder(book))
+    orderListCards.append(...cards);
+    return orderListCards;
+}
+
+function showCart() {
+    if (cart.size) {
+        clearMain();
+        main.append(order());
+    }
+}
+
+function createField(id, name, labelName, type, size) {
+    const field = createElement('div', 'field');
+    const label = createElement('label');
+    label.for = id;
+    label.innerHTML = labelName;
+    const input = createElement('input');
+    input.name = name;
+    input.id = id;
+    input.type = type;
+    input.size = size;
+    field.append(label, input);
+    return field;
+}
+
+function createRadio(id, value) {
+    const element = createElement('input');
+    element.type = 'radio';
+    element.Id = id;
+    element.name = 'choose';
+    element.value = value;
+    return element
+}
+
+function createLabel(forElement, text) {
+    const label = createElement('label');
+    label.for = forElement;
+    label.innerHTML = text;
+    return label;
+}
+
+function createFieldGift(value, labelName) {
+    const element = createElement('div');
+    const input = createElement('input');
+    input.type = 'checkbox';
+    input.id = value;
+    input.name = 'interest';
+    input.value = value;
+    const label = createElement('label');
+    label.for = value;
+    label.innerHTML = labelName;
+    element.append(input, value);
+    return element;
+}
+
+function createCheckbox() {
+    const checkbox = createElement('fieldset', 'fieldset');
+
+    const checkboxLegend = createElement('legend');
+    checkboxLegend.innerHTML = 'Choose 2 gifts: (optional)';
+
+    const giftPack = createFieldGift('pack', 'Pack as a gift');
+    const addPostcard = createFieldGift('postcard', 'Add postcard');
+    const discount = createFieldGift('discount', 'Provide 2% discount to the next time');
+    const branded = createFieldGift('discount', 'Provide 2% discount to the next time');
+
+    checkbox.append(checkboxLegend, giftPack, addPostcard, discount, branded);
+    return checkbox;
+}
+
+function createFormButton() {
+    const formBtn = createElement('button', 'form-btn');
+    formBtn.type = 'submit';
+    formBtn.value = 'Complete';
+    formBtn.innerHTML = 'Submit';
+    return formBtn;
+}
+
+function createFieldsetPyament() {
+    const fieldset = createElement('fieldset', 'fieldset');
+
+    const title = createElement('legend');
+    title.innerHTML = 'Choose payment type:';
+
+    const paymentType = createElement('div', 'payment-type');
+
+    const cashInput = createRadio('contactChoice1', 'cash');
+    const cashInputLabel = createLabel('contactChoice1', 'Cash');
+
+    const cardInput = createRadio('contactChoice2', 'card');
+    const cardInputLabel = createLabel('contactChoice2', 'Card');
+
+    paymentType.append(cashInput, cashInputLabel, cardInput, cardInputLabel);
+    fieldset.append(title, paymentType);
+    return fieldset;
+}
+
+function createFormOrder() {
+    const form = createElement('div', 'form');
+
+    const orderName = createElement('h2', 'order-name');
+    orderName.innerHTML ='ORDER FORM';
+
+    const formBegin = createElement('form', 'form-begin');
+
+    const fieldName = createField('form-name', 'name', 'Name:', 'text', 40);
+    const fieldSurname = createField('form-surname', 'surname', 'Surname:', 'text', 40);
+    const fieldDeliveryDate = createField('form-date', 'delivery date', 'Delivery date:', 'date', 10);
+    const fieldStreet = createField('form-street', 'street', 'Street:', 'text', 40);
+    const fieldHouseNumber = createField('form-number', 'house-number', 'House number:', 'number', 40);
+    const fieldFlatNumber = createField('form-number', 'flat-number', 'Flat number:', 'number', 40);
+
+    const fieldset = createFieldsetPyament();
+
+    const checkbox = createCheckbox();
+
+    const formBtn = createFormButton();
+
+    formBegin.append(fieldName, fieldSurname, fieldDeliveryDate, fieldStreet, fieldHouseNumber, fieldFlatNumber, fieldset, checkbox, formBtn);
+
+    form.append(orderName,formBegin);
+    return form;
+}
+
+function showOrderForm() {
+    clearMain();
+    main.append(createFormOrder());
 }
 
 start();
